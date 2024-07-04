09c39f87061cc5463fbdd27f558fa113649880be
lamlados <lamlados.h@gmail.com>
2024-07-04 16:10:18 +0800

    style: Make header fixed to center

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,20 +3,20 @@ import Link from 'next/link';
 
 export const Header = () => {
     return (
-        <header className='fixed w-full justify-center top-2 z-50'
+        <header className='fixed w-5/6 justify-center top-0 z-50'
         >
-            <div className='w-5/6 items-center shadow-md'>
+            <div className='w-fulljustify-center shadow-md'>
                 <div className='flex items-center justify-between'>
                     <Link href='https://www.google.com'>
-                        <div className='text-lg font-bold text-primary'>Logo</div>
+                        <div className='flex items-center'>Logo</div>
                     </Link>
                     <Link href='https://www.google.com'>
-                        <div className='text-lg font-bold text-primary'>Logo2</div>
+                        <div className='flex items-center'>Logo2</div>
                     </Link>
                 </div>
                 <div className='flex items-center justify-between'>
                     <Link href='https://www.google.com'>
-                        <div className='text-lg font-bold text-primary'>Home</div>
+                        <div className='flex items-center'>Home</div>
                     </Link>
                 </div>
             </div>
